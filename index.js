e63d3bc6ea44369e1de24d1424fd9db84d9bf682
shail611 <85347670+shail611@users.noreply.github.com>
2023-10-19 03:27:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 const expressLayouts = require('express-ejs-layouts');
 
